682295295406283c0f588b4eb030e7b51554a5e9
crazyserega1994 <crazyserega1994@mail.ru>
2011-08-14 17:22:05 +0000

    Nuff said

--- spec-list/client.lua
+++ spec-list/client.lua
@@ -5,7 +5,7 @@ function drawList()
 	local list = getElementData(thePlayer,"spec-list")
 	if list ~= false then
 		if #list ~= 0 then
-			dxDrawText("Spectating:",screenX*.85,screenY*.3,0,0,tocolor(255,0,0,255))
+			dxDrawText("Spectators:",screenX*.85,screenY*.3,0,0,tocolor(255,0,0,255))
 			for i,v in ipairs(list) do
 				if isElement(v) then
 					dxDrawText(getPlayerName(v),screenX*.85,screenY*.3+(i*(screenY*0.02)),0,0,tocolor(0,255,0,255))
