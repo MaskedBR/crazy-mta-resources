495ae39a95a26241de410e12ca4179518439efa5
crazyserega1994@mail.ru <crazyserega1994@mail.ru@8b542a31-81cb-50d6-b787-e55140c502f6>
2011-08-15 12:44:22 +0000

    Some updates

--- trunk/radio-player/client.lua
+++ trunk/radio-player/client.lua
@@ -2,7 +2,8 @@
 	["meta"] = {},
 	["length"] = 0,
 	["pointer"] = nil,
-	["stream_title"] = nil
+	["stream_title"] = nil,
+	["url"] = nil
 }
 
 function soundStarted(success,length,streamName)
@@ -13,25 +14,26 @@ function soundStarted(success,length,streamName)
 		if length == 0 then
 			outputChatBox('#FFFF00RADIO: #FFFFFF"'..streamName..'" stream started.',255,0,0,true)
 			if currentSound.meta.title then
-				outputChatBox('#FFFF00*Title: #FFFFFF"'..currentSound.meta.title,255,0,0,true)
+				outputChatBox('#000000*#FFFF00Title: #FFFFFF"'..currentSound.meta.title,255,0,0,true)
 			end
 			if currentSound.meta.artist then
-				outputChatBox('#FFFF00*Artist: #FFFFFF"'..currentSound.meta.artist,255,0,0,true)
+				outputChatBox('#000000*#FFFF00Artist: #FFFFFF"'..currentSound.meta.artist,255,0,0,true)
 			end
 			if currentSound.meta.stream_name then
-				outputChatBox('#FFFF00*Stream name: #FFFFFF"'..currentSound.meta.stream_name,255,0,0,true)
+				outputChatBox('#000000*#FFFF00Stream name: #FFFFFF"'..currentSound.meta.stream_name,255,0,0,true)
 			end
 			if currentSound.meta.stream_title then
-				outputChatBox('#FFFF00*Stream title: #FFFFFF"'..currentSound.meta.stream_title,255,0,0,true)
+				outputChatBox('#000000*#FFFF00Stream title: #FFFFFF"'..currentSound.meta.stream_title,255,0,0,true)
 			elseif currentSound.stream_title then
-				outputChatBox('#FFFF00*Stream title: #FFFFFF"'..currentSound.stream_title,255,0,0,true)
+				outputChatBox('#000000*#FFFF00Stream title: #FFFFFF"'..currentSound.stream_title,255,0,0,true)
 			end
 		else
-			if currentSound.meta.title then
+			outputChatBox("*********************************",0,0,0)
+			--[[if currentSound.meta.title then
 				outputChatBox('#FFFF00RADIO: #FFFFFF"'..currentSound.meta.title..'" sound started.',255,0,0,true)
 			else
 				outputChatBox('#FFFF00RADIO: #FFFFFFSound started.',255,0,0,true)
-			end
+			end]]--
 			if currentSound.meta.title then
 				outputChatBox('#000000*#FFFF00Title: #FFFFFF'..currentSound.meta.title,255,0,0,true)
 			end
@@ -39,12 +41,12 @@ function soundStarted(success,length,streamName)
 				outputChatBox('#000000*#FFFF00Artist: #FFFFFF'..currentSound.meta.artist,255,0,0,true)
 			end
 			outputChatBox('#000000*#FFFF00Length: #FFFFFF'..tostring(getRealTime(length/1000).minute)..":"..tostring(getRealTime(length/1000).second),255,0,0,true)
+			outputChatBox("*********************************",0,0,0)
 		end
 	else
 		outputChatBox("#FFFF00RADIO: #FFFFFFFailed to play sound/stream.",255,0,0,true)
 	end
 end
-
 
 
 function startSound(url)
@@ -56,15 +58,59 @@ function startSound(url)
 		["meta"] = {},
 		["length"] = 0,
 		["pointer"] = nil,
+		["url"] = nil
 	}
+	currentSound.url = url
 	playSound(url)
 end
 
 function onSoundStop()
-	outputChatBox("Stopped!")
 	if currentSound.length ~= 0 then
-		
+		if guiCheckBoxGetSelected(GUIEditor_Checkbox[2]) == true then
+			startSound(currentSound.url)
+		else
+			currentSound = {
+				["meta"] = {},
+				["length"] = 0,
+				["pointer"] = nil,
+				["url"] = nil
+			}
+		end
+	else
+		currentSound = {
+			["meta"] = {},
+			["length"] = 0,
+			["pointer"] = nil,
+			["url"] = nil
+		}
+		outputChatBox("#FFFF00RADIO: #FFFFFFStream stopped.",255,0,0,true)
 	end
+end
+
+function onSoundChange(title)
+	currentSound.meta = getSoundMetaTags(source)
+	currentSound.stream_title = title
+	if currentSound.stream_title then
+		outputChatBox("#FFFF00RADIO: #FFFFFFNow playing "..currentSound.stream_title,255,0,0,true)
+	else
+		outputChatBox("#FFFF00RADIO: #FFFFFFNow playing "..currentSound.url,255,0,0,true)
+	end
+	outputChatBox("*********************************",0,0,0)
+	if currentSound.meta.title then
+		outputChatBox('#000000*#FFFF00Title: #FFFFFF"'..currentSound.meta.title,255,0,0,true)
+	end
+	if currentSound.meta.artist then
+		outputChatBox('#000000*#FFFF00Artist: #FFFFFF"'..currentSound.meta.artist,255,0,0,true)
+	end
+	if currentSound.meta.stream_name then
+		outputChatBox('#000000*#FFFF00Stream name: #FFFFFF"'..currentSound.meta.stream_name,255,0,0,true)
+	end
+	if currentSound.meta.stream_title then
+		outputChatBox('#000000*#FFFF00Stream title: #FFFFFF"'..currentSound.meta.stream_title,255,0,0,true)
+	elseif currentSound.stream_title then
+		outputChatBox('#000000*#FFFF00Stream title: #FFFFFF"'..currentSound.stream_title,255,0,0,true)
+	end
+	outputChatBox("*********************************",0,0,0)
 end
 
 --Controls start
@@ -74,6 +120,8 @@ function ()
 		local curVolume = getSoundVolume(currentSound.pointer)
 		if curVolume < 1 then
 			setSoundVolume(currentSound.pointer,curVolume+0.1)
+		else
+			setSoundVolume(currentSound.pointer,1)
 		end
 	end
 end)
@@ -84,6 +132,8 @@ function ()
 		local curVolume = getSoundVolume(currentSound.pointer)
 		if curVolume > 0 then
 			setSoundVolume(currentSound.pointer,curVolume-0.1)
+		else
+			setSoundVolume(currentSound.pointer,0)
 		end
 	end
 end)
@@ -100,10 +150,12 @@ function ()
 				triggerEvent("onClientSoundStop",currentSound.pointer)
 			end
 		else
-			if not getSoundPosition(currentSound.pointer) then
+			if getSoundPosition(currentSound.pointer) == 0 then
 				triggerEvent("onClientSoundStop",currentSound.pointer)
 			end
 		end
 	end
 end,1000,0)
+addEventHandler("onClientSoundStop",getResourceRootElement(getThisResource()),onSoundStop)
+addEventHandler("onClientSoundChangedMeta",getResourceRootElement(getThisResource()),onSoundChange)
 --Events end
